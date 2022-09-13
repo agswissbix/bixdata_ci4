c47cb4c07aad531d3d7e0770ddc61b736bbfb5b0
agswissbix <108857221+agswissbix@users.noreply.github.com>
2022-09-13 15:26:33 +0200

    .

--- project-root/app/Views/BixView/Results.php
+++ project-root/app/Views/BixView/Results.php
@@ -8,9 +8,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 
-<!--Custom css
-<link href="../css/sb-admin-2.css" rel="stylesheet">
--->
 
 <!--Font google-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
