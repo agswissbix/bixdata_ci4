df47cec886cc15908819943c59b80c844d8128a0
agswissbix <108857221+agswissbix@users.noreply.github.com>
2022-09-19 16:11:57 +0200

    .

--- project-root/app/Views/BixView/Login.php
+++ project-root/app/Views/BixView/Login.php
@@ -29,11 +29,11 @@
         name: 'password',
         value: $('#password').val()
       });
-      
+
       $("#base_container").load('Loading.php');      
       $.ajax({
         type: "POST",
-        url: controller_url + 'ajax_login',
+        url: controller_url + 'ajax_login', 
         data: serialized_data,
         success: function(response) {
           $("#base_container").html(response);
