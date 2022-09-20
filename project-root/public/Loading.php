7c063b22c8a835bac6d5804a45e72af188f9d6e7
marco <marco.garganigo@swissbix.ch>
2022-09-20 11:36:25 +0200

    .

--- project-root/public/Loading.php
+++ project-root/public/Loading.php
@@ -1,17 +1,91 @@
 <style type="text/css">
-    .spinner-border {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(50%, -50%);
+    .loader {
+        border: 16px solid #f3f3f3;
+        /* Light grey */
+        border-top: 16px solid #048023;
+        /* Dark Green */
+        border-radius: 50%;
+        width: 120px;
+        height: 120px;
+        animation: spinloader 2s linear infinite;
+    }
+
+    .loader img {
+        height: 120px;
+        width: 120px;
+        animation: spinlogo 2s linear infinite;
+    }
+
+    @keyframes spinloader {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    @keyframes spinlogo {
+        0% {
+            transform: rotate(360deg);
+        }
+
+        100% {
+            transform: rotate(0deg);
+        }
     }
 </style>
 
 
-<div class="spinner-border" role="status">
-    <span class="visually-hidden">Loading...</span>
-</div>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="spinner-grow" role="status">
-    <span class="visually-hidden">Loading...</span>
-</div>
+<head>
+    <style type="text/css">
+        .loader {
+            border: 16px solid #f3f3f3;
+            /* Light grey */
+            border-top: 16px solid #048023;
+            /* Dark Green */
+            border-radius: 50%;
+            width: 120px;
+            height: 120px;
+            animation: spinloader 2s linear infinite;
+        }
+
+        .loader img {
+            height: 120px;
+            width: 120px;
+            animation: spinlogo 2s linear infinite;
+        }
+
+        @keyframes spinloader {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+
+        @keyframes spinlogo {
+            0% {
+                transform: rotate(360deg);
+            }
+
+            100% {
+                transform: rotate(0deg);
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="loader">
+        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20210610212340/gfglogo.png" />
+    </div>
+</body>
+
+</html>
