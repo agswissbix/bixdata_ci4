06bffd1b0b52ecddd62a74dae4e1dc233bd673a6
marco <marco.garganigo@swissbix.ch>
2022-09-20 16:50:52 +0200

    -

--- project-root/app/Views/BixView/RecordCard.php
+++ project-root/app/Views/BixView/RecordCard.php
@@ -1,8 +1,41 @@
 <div id="tables" class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">REcord example</h6>
-    </div>
+
     <div class="card-body">
-        Body example
-    </div>
-</div>
+
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button class="btn btn-primary me-md-2" type="button" style="margin-right: 10px;">Stampe</button>
+            <button class="btn btn-primary" type="button">Funzioni</button>
+        </div>
+
+        <div class="container text-center">
+            <div class="row row-cols-3">
+                <div class="col"><b>Document nr
+                        </br>
+                    </b></div>
+                <div class="col"><b>Company
+                        </br>
+                    </b></div>
+                <div class="col"><b>Title
+                        </br>
+                    </b></div>
+                <div class="col"><b>Total net yearly
+                        </br>
+                    </b></div>
+
+            </div>
+        </div>
+
+        <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+                <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+                <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+                <button class="nav-link" id="nav-disabled-tab" data-bs-toggle="tab" data-bs-target="#nav-disabled" type="button" role="tab" aria-controls="nav-disabled" aria-selected="false" disabled>Disabled</button>
+            </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">...</div>
+            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">...</div>
+            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">...</div>
+            <div class="tab-pane fade" id="nav-disabled" role="tabpanel" aria-labelledby="nav-disabled-tab" tabindex="0">...</div>
+        </div>
