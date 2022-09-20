af52eaeb1a610449014a29ff739a16dc92a23432
agswissbix <108857221+agswissbix@users.noreply.github.com>
2022-09-20 08:29:08 +0200

    .

--- project-root/app/Views/BixView/Login.php
+++ project-root/app/Views/BixView/Login.php
@@ -35,10 +35,10 @@
       url: controller_url + 'ajax_login',
       data: serialized_data,
       success: function(response) {
-        // $("#base_container").html(response);
+         $("#base_container").html(response);
       },
       error: function() {
-        // $("#base_container").html(response);
+         $("#base_container").html(response);
       }
     });
 
