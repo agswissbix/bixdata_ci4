88265bfbb7c26dab3e1efd79c41e8f6bc976e7af
marco <marco.garganigo@swissbix.ch>
2022-09-23 13:54:45 +0200

    .

--- project-root/app/Views/BixView/RecordCard.php
+++ project-root/app/Views/BixView/RecordCard.php
@@ -5,33 +5,33 @@
 
     <div class="card-body">
 
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end" style="margin-bottom: 15px">
 
-            <button type="button" class="btn btn-secondary btn-sm">
+            <button type="button" class="btn btn-outline btn-sm">
                 <span class="material-symbols-outlined">
                     content_copy
                 </span>
 
             </button>
-            <button type="button" class="btn btn-danger btn-sm">
+            <button type="button" class="btn btn-outline btn-sm" style="color: red">
                 <span class="material-symbols-outlined">
                     delete
                 </span>
             </button>
 
-            <button type="button" class="btn btn-secondary btn-sm">
+            <button type="button" class="btn btn-outline btn-sm">
                 <span class="material-symbols-outlined">
                     lock
                 </span>
             </button>
 
-            <button type="button" class="btn btn-secondary btn-sm">
+            <button type="button" class="btn btn-outline btn-sm">
                 <span class="material-symbols-outlined">
                     push_pin
                 </span>
             </button>
 
-            <button type="button" class="btn btn-secondary btn-sm">
+            <button type="button" class="btn btn-outline btn-sm">
                 <span class="material-symbols-outlined">
                     close
                 </span>
@@ -46,20 +46,38 @@
             <button class="btn btn-outline-secondary" type="button">Funzioni</button>
         </div>
 
+        <br>
+
         <div class="container text-center">
-            <div class="row row-cols-3">
+            <div class="row row-cols-2">
                 <div class="col"><b>Document nr
-                        </br>
-                    </b></div>
+
+                    </b>
+                    </br>
+                    123456
+                </div>
                 <div class="col"><b>Company
-                        </br>
-                    </b></div>
-                <div class="col"><b>Title
-                        </br>
-                    </b></div>
-                <div class="col"><b>Total net yearly
-                        </br>
-                    </b></div>
+
+                    </b>
+                    </br>
+                    Swissbix SA
+                </div>
+                <div class="col">
+                    </br>
+                    <b>
+                        Title
+
+                    </b>
+                    </br>
+                    abc
+                </div>
+                <div class="col">
+                    </br>
+                    <b>Total net yearly
+                    </b>
+                    </br>
+                    123456
+                </div>
 
             </div>
         </div>
@@ -70,20 +88,22 @@
                 <button class="nav-link" id="nav-allegati-tab" data-bs-toggle="tab" data-bs-target="#nav-allegati" type="button" role="tab" aria-controls="nav-allegati" aria-selected="false">Allegati</button>
             </div>
         </nav>
-
+        </br>
         <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="dati-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
 
                 <p>
-                    <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" style="">
+                    <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                         Sales order line
                     </a>
                     <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" style=" margin-top: 5px">
                         Service and asset
                     </button>
+                    </br>
                     <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" style=" margin-top: 5px">
                         Company
                     </button>
+                    </br>
                     <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" style=" margin-top: 5px">
                         Dati
                     </button>
@@ -98,6 +118,6 @@
             <div class="tab-pane fade" id="allegati-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
         </div>
 
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end" style="margin-top: 10px">
             <button type="button" class="btn btn-secondary">Chiudi</button>
         </div>
