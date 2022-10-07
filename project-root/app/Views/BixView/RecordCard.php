d75a721b9890f99a5a2915d50f902c9f4520736e
marco <marco.garganigo@swissbix.ch>
2022-10-07 14:20:37 +0200

    -

--- project-root/app/Views/BixView/RecordCard.php
+++ project-root/app/Views/BixView/RecordCard.php
@@ -1,4 +1,47 @@
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
 
-card
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <p class="card-text">
+
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end" style="margin-bottom: 15px">
+
+            <button type="button" class="btn btn-outline btn-sm">
+                <span class="material-symbols-outlined">
+                    content_copy
+                </span>
+
+            </button>
+            <button type="button" class="btn btn-outline
+         btn-sm" style="color: red">
+                <span class="material-symbols-outlined">
+                    delete
+                </span>
+            </button>
+
+            <button type="button" class="btn btn-outline btn-sm">
+                <span class="material-symbols-outlined">
+                    lock
+                </span>
+            </button>
+
+            <button type="button" class="btn btn-outline btn-sm">
+                <span class="material-symbols-outlined">
+                    push_pin
+                </span>
+            </button>
+
+            <button type="button" class="btn btn-outline btn-sm">
+                <span class="material-symbols-outlined">
+                    close
+                </span>
+            </button>
+
+        </div>
+
+        </p>
+    </div>
+</div>
