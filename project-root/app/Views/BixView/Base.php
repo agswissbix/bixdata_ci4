1aa2e2639ef67da2e035f655d0cd20fc2d8a70b9
marco <marco.garganigo@swissbix.ch>
2022-11-08 13:10:51 +0100

    commit

--- project-root/app/Views/BixView/Base.php
+++ project-root/app/Views/BixView/Base.php
@@ -14,7 +14,8 @@ helper('base_helper');
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <!--Fullcalendar-->
-
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.css' rel='stylesheet' />
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.js"></script>
 
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
